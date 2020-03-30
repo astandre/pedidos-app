821c6a34341af75b663bf3c8f676699c356f966f
André Herrera <andreherrera97@hotmail.com>
2020-03-30 00:27:04 -0500

    Adding router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { PedidosManageComponent } from './component/pedidos-manage/pedidos-manage.component';
+import {AppComponent} from './app.component';
+import {PedidosManageComponent} from './component/pedidos-manage/pedidos-manage.component';
 import {HttpClientModule} from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'pedido', component: PedidosManageComponent},
+  // {path: 'train', component: UnclassifiedSentencesComponent},
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PedidosManageComponent
   ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        ReactiveFormsModule,
-    ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    )
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
